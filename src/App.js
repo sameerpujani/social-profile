c34e120472122a44bd0e3dd33086e40ecf3a0e6a
sameerpujani <sameerpujanigndpc@gmail.com>
2019-12-22 23:37:54 +0530

    Profile, Gallery and Todo functionality - First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Users from "./Components/Users";
+import Profile from "./Components/Profile";
+import Posts from "./Components/Profile/Posts";
+import Gallery from "./Components/Profile/Gallery";
+import Album from "./Components/Profile/Album";
+import Todo from "./Components/Profile/Todo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/user/:id/profile" component={Profile} />
+          <Route path="/user/:id/posts" component={Posts} />
+          <Route exact path="/user/:id/gallery" component={Gallery} />
+          <Route path="/user/:id/gallery/:album_id" component={Album} />
+          <Route path="/user/:id/todo" component={Todo} />
+
+          {/* Default route when nothing else matches */}
+          <Route component={Users} />
+        </Switch>
+      </Router>
+    );
+  }
 }
-
-export default App;
